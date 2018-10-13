5116fccb3c8a017dac804a9c9e7326225430ac1f
XESC <xesc87@msn.com>
2018-10-13 03:04:44 +0200

    13/10/2018

--- app/src/main/java/com/rumba/prowling/LoginActivity.java
+++ app/src/main/java/com/rumba/prowling/LoginActivity.java
@@ -172,7 +172,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     /**
-     * Callback received when a permissions request has been completed.
+     * Callback received when a permissions request has been completed...
      */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
