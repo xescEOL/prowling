b9dce976636fc53437e99fd8d4db7e9a7d3e2e29
XESC <xesc87@msn.com>
2018-10-14 21:06:05 +0200

    Octubre

--- app/src/main/java/com/rumba/prowling/MatchActivity.java
+++ app/src/main/java/com/rumba/prowling/MatchActivity.java
@@ -2,12 +2,9 @@ package com.rumba.prowling;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class MatchActivity extends Fragment {
     @Override
